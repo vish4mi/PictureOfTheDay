f677b2f009f5ab8c2a5dc3d31f431ecf3cb5531b
Vishal Bhadade <vishal.bhadade@gmail.com>
2022-04-25 20:27:02 +0530

    Initial commit

---
+++ PictureOfTheDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PictureOfTheDay
+//
+//  Created by Vishal on 25/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
